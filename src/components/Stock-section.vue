d98abcb1d4b6e533c1fe100fc0997460d495c549
Hisham Salameh <salamehh@msu.edu>
2022-04-13 23:15:39 -0400

    complete

--- src/components/Stock-section.vue
+++ src/components/Stock-section.vue
@@ -69,7 +69,7 @@ export default {
       loadInfoContainer: false,
       isLoading: false,
       errorClass: false,
-      loadingImage: require('../images/loading.gif'),
+      loadingImage: require('../images/stock.gif'),
       // DATA RELATED TO RECENTLY VIEWED SECTION IN UI
       recentlyViewed: [],
       // DYNAMIC STYLES
